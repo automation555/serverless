59bdb0d649ba1c57044390037990843052a42d4a
automation555@mygamma.io <automation555@mygamma.io>
2022-07-03 00:32:54 +0530

    C-fix: aws-s3-accelerate for package fixes #9812

--- test/unit/lib/cli/filter-supported-options.test.js
+++ test/unit/lib/cli/filter-supported-options.test.js
@@ -50,6 +50,7 @@ describe('test/unit/lib/cli/filter-supported-options.test.js', () => {
     ).to.deep.equal({
       'region': 'elo',
       'aws-profile': null,
+      'aws-s3-accelerate': null,
       'help': null,
       'version': null,
       'config': null,
@@ -57,8 +58,6 @@ describe('test/unit/lib/cli/filter-supported-options.test.js', () => {
       'app': null,
       'org': null,
       'use-local-credentials': null,
-      'verbose': null,
-      'debug': null,
     });
   });
 
@@ -79,8 +78,6 @@ describe('test/unit/lib/cli/filter-supported-options.test.js', () => {
       version: null,
       config: null,
       stage: 'marko',
-      verbose: null,
-      debug: null,
     });
   });
 });
